d857f837bd7ff5779933491a53ca2a1b2c853e4d
NathanScott85 <contact@nathanscott.net>
2020-05-20 20:18:17 +0100

    set up routes and header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/header/index';
+import Routes from './app-routes';
+
+function App() {
+  return (
+    <Routes>
+      <Header />
+    </Routes>
+
+  );
+}
+
+export default App;
