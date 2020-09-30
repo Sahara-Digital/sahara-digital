ec43175dcd64f60d68425428f87149d90bf4eae2
NathanScott85 <contact@nathanscott.net>
2020-09-30 16:16:06 +0100

    removed react

--- src/assets/svg/index.jsx
+++
@@ -1,1 +0,0 @@
-export { default as Down} from './down-arrow';
--- src/components/pages/index.jsx
+++
@@ -1,3 +0,0 @@
-export { default as About } from "./about";
-export { default as Contact } from './contact';
-export { default as Events } from './events';
