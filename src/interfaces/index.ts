3440395a252a3a6cfb424a8937632789bdda60c7
NathanScott85 <contact@nathanscott.net>
2020-10-12 14:52:48 +0100

    added interfaces svg, removed unused import

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -6,3 +6,10 @@ export interface IHeaderNavigation {
     history: any;
     children?: any;
 }
+
+export interface ISVG {
+    viewBox?: string;
+    width?: string;
+    height?: string;
+    fill?: string;
+}
