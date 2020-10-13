ee972bbcc857dd2bbbdadb57dae9c6693d207f59
NathanScott85 <contact@nathanscott.net>
2020-10-13 19:30:25 +0100

    added navigation mobile responsive

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { MenuLink } from '../';
+import media from 'styled-media-query';
 
 export const HeaderNavigation = () => {
     return (
@@ -19,4 +20,13 @@ const Container = styled.div`
     padding: 1rem;
     display: flex;
     justify-content: space-evenly;
+    ${media.lessThan('small')`
+    overflow: hidden;
+        margin: 0;
+        a {
+            ${({ theme }) => `font-size:${theme.fontsize.small}`};
+        }
+        flex-direction: column;
+        padding: 0;
+    `}
 `;
