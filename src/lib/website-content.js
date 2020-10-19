ea3522972ef1e40a8866a11d616dd46407d90b56
NathanScott85 <contact@nathanscott.net>
2020-10-19 14:27:31 +0100

    added lib/ website content

---
+++ src/lib/website-content.js
@@ -0,0 +1,45 @@
+export const values = [
+    {
+        id: '1',
+        title: 'Why choose us?',
+        name: 'Integrity',
+        paragraph: 'We value honesty and uphold strong moral principles',
+    },
+    {
+        id: '2',
+        name: 'Teamwork',
+        paragraph:
+            'We focus on working collaboratively to achieve our common goal effectively, providing effective Web Development and software development services to a broad spectrum of clients. Teamwork is at the heart of what we do.',
+    },
+    {
+        id: '3',
+        name: 'Quality',
+        paragraph:
+            'We are dedicated to providing a high standard of services across the board, making us competitive with other companies in our field. We understand that clean, good-quality code makes the software more robust, usable, and easier to maintain.',
+    },
+    {
+        id: '4',
+        name: 'Simplicity',
+        paragraph:
+            '‘Simple is beautiful’ is a programming mantra we embrace, by embracing the beauty of simplicity, we commit to uncomplicated and design-focused websites and applications.',
+    },
+    {
+        id: '5',
+        name: 'Passion',
+        paragraph:
+            'We love what we do, and we are fuelled by a mutual enthusiasm to make premium quality solutions for a range of customers.',
+    },
+];
+
+export const introduction = [
+    {
+        id: '1',
+        pageTitle: 'About us',
+        paragraph: `We specialize in web and software development practices, our aim is to provide a full-stack service in the future, which will allow focus on the client or customers' needs and let the innovation takeplace naturally throughout the software development life cycle.`,
+    },
+    {
+        id: '2',
+        paragraph:
+            'Our core business structure involves a system called OKR; (Objectives and Key Results) we use this system to align ourselves with our companies objectives, our clients’ objectives, or even a developer’s learning objectives. This is what makes us different from other businesses in our sector.',
+    },
+];
