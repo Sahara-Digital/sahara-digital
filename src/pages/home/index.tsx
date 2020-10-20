754f935e6926ae042849d351aac528c78d1a05a1
NathanScott85 <contact@nathanscott.net>
2020-10-20 16:46:15 +0100

    removed services content

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import media from 'styled-media-query';
 import {
-    // Card,
     Navigation,
     HeaderNavigation,
     Footer,
@@ -10,11 +9,10 @@ import {
     BurgerMenu,
     Menu,
 } from '../../components';
-// import { ShapeFour, ShapeThree } from '../../components/image/svg';
 import { IPagesProps } from '../../interfaces';
 import { customMedia } from '../../lib';
 import { homepageIntro } from '../../lib/website-content';
-//homepage
+
 export const Home = ({ history }: IPagesProps) => {
     const [open, setOpen] = React.useState(false);
     return (
@@ -39,20 +37,6 @@ export const Home = ({ history }: IPagesProps) => {
                     <Shapes />
                 </Content>
 
-                {/* <CardContainer>
-                    {
-                      homepage.map((content: any)=> {
-                        return (
-                           content.title &&  <Card
-                           title={content.title}
-                           component={content.title === "Web Consulting" ? <ShapeFour width="10rem" /> : <ShapeThree width="10rem" />}
-                           paragraph={content.paragraph}
-                       /> 
-                        )
-                      })
-                    }
-
-                </CardContainer> */}
             </Main>
             <Footer />
         </>
@@ -69,21 +53,6 @@ const Paragraph = styled.p`
         }
   `}
 `;
-// const CardContainer = styled.section`
-//     display: flex;
-//     padding: 2rem;
-//     margin: 2rem;
-//     flex-direction: row;
-//     justify-content: center;
-//     ${media.lessThan('small')`
-//        padding: 1rem 1rem 1rem 1rem;
-//        margin: 0;
-//        flex-direction: column;
-//         h1{
-//             ${({ theme }) => `font-size:${theme.fontsize.xxlarge}`};
-//         }
-//   `}
-// `;
 const Content = styled.section`
     width: 100%;
     display: flex;
