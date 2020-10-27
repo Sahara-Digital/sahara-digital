5b5a7e1a6fd326faaa220df6a0664c9a5e5e7704
NathanScott85 <contact@nathanscott.net>
2020-10-27 16:17:06 +0000

    Updates

---
+++ precache-manifest.605c56da6d547651edd8fbb7fab5f9d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8230eb60689fc37e2b131e3518d33953",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1247f220e7d263925e3",
+    "url": "/static/css/main.31bc4ab1.chunk.css"
+  },
+  {
+    "revision": "9e445202975f0b0430f7",
+    "url": "/static/js/2.8bb1932a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.8bb1932a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1247f220e7d263925e3",
+    "url": "/static/js/main.4e3563df.chunk.js"
+  },
+  {
+    "revision": "438b0affdf658c4d26f8",
+    "url": "/static/js/runtime-main.65beef02.js"
+  },
+  {
+    "revision": "e65d445c4305ef4925e6c38f40b1e126",
+    "url": "/static/media/sahara-digital.e65d445c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sahara-digital/precache-manifest.040d03ca84ddbb3fcf984cf0c32458f9.js"
+  "/precache-manifest.605c56da6d547651edd8fbb7fab5f9d9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sahara-digital/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
