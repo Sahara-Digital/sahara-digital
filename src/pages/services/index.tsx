1a8d89c6c0b19e5181586d788f0224d471e21ea0
Nathan Scott <contact@nathanscott.net>
2021-03-08 13:19:17 +0000

    fixed production issue with duplicate identifier

--- src/pages/services/index.tsx
+++ src/pages/services/index.tsx
@@ -141,31 +141,7 @@ export const Services: React.FunctionComponent<IPagesProps> = ({
 const ContactLink = styled(StyledLink)`
     padding-left: 0.2rem;
 `
-const DigitalTransformation = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-    align-items: center;
-    border-top: 1px solid #9ccccc;
-    width: 100%;
-    margin: 1rem;
-    padding: 1rem;
-    h2 {
-        margin-top: 1rem;
-    }
-    svg {
-        margin: 1rem;
-    }
-    ${media.lessThan('small')`
-       padding: 1rem 1rem 1rem 1rem;
-       margin: 0;
-       flex-direction: column;
-       svg {
-        margin: 0;
-        width: 75%;
-    }
-  `}
-`;
+
 const OnlineWebPresence = styled.div`
     display: flex;
     flex-direction: row;
@@ -217,32 +193,6 @@ const DigitalTransformation = styled.div`
         width: 75%;
     }
   `}
-`;
-const OnlineWebPresence = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-    align-items: center;
-    border-top: 1px solid #9ccccc;
-    width: 100%;
-    margin: 1rem;
-    padding: 1rem;
-    h2 {
-        margin-top: 1rem;
-    }
-    svg {
-        margin: 1rem;
-        width: 50%;
-    }
-    ${media.lessThan('small')`
-       padding: 1rem 1rem 1rem 1rem;
-       margin: 0;
-       flex-direction: column-reverse;
-       svg {
-        margin: 0;
-        width: 75%;
-    }
-  `}    
 `;
 
 const ServicesMain = styled.main`
